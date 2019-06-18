63ed58a924a550a2a6ddc007b46b6bc62a219e54
Amy Westcott <amy.westcott@yahoo.com>
2019-06-18 04:58:13 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,43 @@ require 'pry'
 class String
 
   def sentence?
-    
-  end
+    if self.end_with?(".")
+      true 
+    else 
+    false 
+  end 
+    end
 
   def question?
-
+if self.end_with?("?")
+      true 
+    else 
+    false 
   end
-
+end
   def exclamation?
-
+    if self.end_with?("!")
+      true 
+    else 
+    false 
   end
-
+end
   def count_sentences
+  self_array = self.split("")
+  remove_extra = String.new
+#   self_array.each_with_index do |word, index|
+#   if word == remove_extra
+#     self_array[index].pop
+#   end 
+#   index += 1
+#   remove_extra = word
+# end
+   self_array.each do |word|
+   if word != "." || word != "!" || word != "?"
+     self_array.delete(word)
+   end
+ end
+   self_array.count
+end
 
-  end
 end
