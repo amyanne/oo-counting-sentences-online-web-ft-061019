b0f82f15b89d2fa3da1e0f8051bb68cfba632708
Amy Westcott <amy.westcott@yahoo.com>
2019-06-18 05:12:20 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -28,9 +28,12 @@ end
   self_array = self.split("")
   count_array = []
   repeat_check = String.new
-   self_array.each do |word|
+   self_array.each_with_index do |word, index|
    if word == repeat_check
-     self_array.pop
+     self_array.pop(index)
+   end 
+   repeat_check = word 
+   index += 1 
    if word.question? || word.sentence? || word.exclamation?
      count_array << word
    end
